20b04fa6342c133888d015512116275b869087c6
Santiago Pastorino <spastorino@gmail.com>
2018-10-18 15:34:40 -0400

    Make args an iterator

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,10 +1,12 @@
 use std::convert::TryFrom;
+use std::ffi::OsStr;
+use std::str::SplitWhitespace;
 
 // A command consists of a binary and its arguments
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 pub struct Cmd<'a> {
-    pub binary: &'a str,
-    pub args: Vec<&'a str>,
+    pub binary: &'a OsStr,
+    pub args: LineIter<'a>,
 }
 
 impl<'a> TryFrom<&'a str> for Cmd<'a> {
@@ -12,11 +14,27 @@ impl<'a> TryFrom<&'a str> for Cmd<'a> {
 
     // Extract the command and its arguments from the commandline
     fn try_from(line: &'a str) -> Result<Self, Self::Error> {
-        let mut parts = line.split_whitespace();
-        let binary = parts.nth(0).ok_or_else(|| ParseError::EmptyLine)?;
-        let args = parts.collect();
+        let mut line_iter = LineIter(line.split_whitespace());
 
-        Ok(Cmd { binary, args })
+        let binary = line_iter.next()
+            .map(OsStr::new)
+            .ok_or(ParseError::EmptyLine)?;
+
+        Ok(Cmd {
+            binary,
+            args: line_iter,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct LineIter<'a>(SplitWhitespace<'a>);
+
+impl<'a> std::iter::Iterator for LineIter<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
     }
 }
 
@@ -36,23 +54,20 @@ mod test {
 
     #[test]
     fn test_single_binary() {
-        assert_eq!(
-            Cmd::try_from("echo").unwrap(),
-            Cmd {
-                binary: "echo",
-                args: vec![]
-            }
-        );
+        let mut cmd = Cmd::try_from("echo").unwrap();
+
+        assert_eq!(cmd.binary, OsStr::new("echo"));
+        assert_eq!(cmd.args.next(), None);
     }
 
     #[test]
     fn test_binary_with_arguments() {
-        assert_eq!(
-            Cmd::try_from("echo 1 2 3").unwrap(),
-            Cmd {
-                binary: "echo",
-                args: vec!["1", "2", "3"]
-            }
-        );
+        let mut cmd = Cmd::try_from("echo 1 2 3").unwrap();
+
+        assert_eq!(cmd.binary, OsStr::new("echo"));
+        assert_eq!(cmd.args.next(), Some("1"));
+        assert_eq!(cmd.args.next(), Some("2"));
+        assert_eq!(cmd.args.next(), Some("3"));
+        assert_eq!(cmd.args.next(), None);
     }
 }
